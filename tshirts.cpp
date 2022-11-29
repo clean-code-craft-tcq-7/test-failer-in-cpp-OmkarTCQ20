965fe207417de91f075cb3abdc882a845fba786c
OmkarTCQ20 <115543848+OmkarTCQ20@users.noreply.github.com>
2022-11-29 16:39:00 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -12,11 +12,16 @@ char size(int cms) {
     }
     return sizeName;
 }
-
-int main() {
+void testFunction()
+{
     assert(size(37) == 'S');
+    assert(size(38) == 'M');
     assert(size(40) == 'M');
+    assert(size(42) == 'L');
     assert(size(43) == 'L');
+}
+int main() {
+    testFunction();
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
