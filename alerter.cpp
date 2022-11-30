38482dfa90ae920f130ea6ccdbe3b647aba5bb1d
OmkarTCQ20 <115543848+OmkarTCQ20@users.noreply.github.com>
2022-11-30 09:07:48 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -10,7 +10,9 @@ int networkAlertStub(float celcius) {
     // stub always succeeds and returns 200
     if(celcius <= 200){
         return 200;
-    }
+      }
+    else 
+        return 500;
   
 }
 
@@ -22,13 +24,14 @@ void alertInCelcius(float farenheit) {
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
 
 int main() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
+    assert(alertFailureCount == 1);
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
